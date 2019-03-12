4ad5f421e867d77342f9b5c41639fc81c6318227
archi-tektur <kontakt@archi-tektur.pl>
2019-03-12 02:35:09 +0100

    Created connector service classes

--- src/Service/RelationService/ArticleAuthorsService.php
+++ src/Service/RelationService/ArticleAuthorsService.php
@@ -1,15 +1,61 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: archi-tektur
- * Date: 3/12/19
- * Time: 12:45 AM
- */
+<?php declare(strict_types=1);
 
 namespace App\Service\RelationService;
 
+use App\Entity\Account;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 
+/**
+ * Author actions on Articles
+ *
+ * @package App\Service\RelationService
+ */
 class ArticleAuthorsService
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Article $article
+     * @param Account $account
+     */
+    public function add(Article $article, Account $account): void
+    {
+        $article->addAuthor($account);
+
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param Article $article
+     * @param Account $account
+     */
+    public function remove(Article $article, Account $account): void
+    {
+        $article->removeAuthor($account);
+
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * Returns true if article has $category, false if not.
+     *
+     * @param Article $article
+     * @param Account $account
+     * @return bool
+     */
+    public function has(Article $article, Account $account): bool
+    {
+        return $article->getAuthors()->contains($account);
+    }
 }
--- src/Service/RelationService/ArticleCategoriesService.php
+++ src/Service/RelationService/ArticleCategoriesService.php
@@ -3,6 +3,60 @@
 namespace App\Service\RelationService;
 
 
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Category actions on Articles
+ *
+ * @package App\Service\RelationService
+ */
 class ArticleCategoriesService
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Article  $article
+     * @param Category $category
+     */
+    public function add(Article $article, Category $category): void
+    {
+        $article->addCategory($category);
+
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param Article  $article
+     * @param Category $category
+     */
+    public function remove(Article $article, Category $category): void
+    {
+        $article->removeCategory($category);
+
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * Returns true if article has $category, false if not.
+     *
+     * @param Article  $article
+     * @param Category $category
+     * @return bool
+     */
+    public function has(Article $article, Category $category): bool
+    {
+        return $article->getCategories()->contains($category);
+    }
 }
