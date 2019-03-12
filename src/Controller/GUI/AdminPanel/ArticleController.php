cb798cc71be5f8f9dddfb0af4f779c25ae5b7dc8
archi-tektur <kontakt@archi-tektur.pl>
2019-03-12 14:45:41 +0100

    Created simple stub of article adder

--- src/Controller/GUI/AdminPanel/ArticleController.php
+++ src/Controller/GUI/AdminPanel/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller\GUI\AdminPanel;
 
 use App\Entity\Article;
 use App\Form\ArticleFormType;
+use App\Service\EntityService\ArticleService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -18,13 +19,28 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ArticleController extends AbstractController
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+    /**
+     * @var ArticleService
+     */
+    protected $articleService;
+
+    public function __construct(EntityManagerInterface $entityManager, ArticleService $articleService)
+    {
+        $this->entityManager = $entityManager;
+        $this->articleService = $articleService;
+    }
+
     /**
      * @Route("/admin/article/add", name="gui__admin_article_add")
-     * @param EntityManagerInterface $em
-     * @param Request                $request
+     * @param Request $request
      * @return RedirectResponse|Response
      */
-    public function add(EntityManagerInterface $em, Request $request)
+    public function add(Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
         $form->handleRequest($request);
@@ -32,9 +48,19 @@ class ArticleController extends AbstractController
 
             /** @var Article $article */
             $article = $form->getData();
-            return $this->redirectToRoute('gui__admin_article_add');
+            return $this->redirectToRoute('gui__admin_article_list');
 
         }
-        return $this->render('base.html.twig', ['form' => $form->createView()]);
+        return $this->render('admin/article/article_add.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/admin/article/list", name="gui__admin_article_list")
+     */
+    public function expose(): Response
+    {
+        return $this->render('admin/article/article_list.html.twig', [
+            'articles' => $this->articleService->getAll(),
+        ]);
     }
 }
--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,9 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Article;
-use App\Entity\Category;
 use App\Service\EntityService\CategoryService;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -34,12 +32,7 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('title')
-                ->add('content')
-                ->add('createdAt', null, ['widget' => 'single_text'])
-                ->add('category', EntityType::class, [
-                    'multiple' => true,
-                    'class'    => Category::class,
-                ]);
+                ->add('content');
     }
 
     /**
