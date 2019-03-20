f569d638dc2c57e0579fa3f41accc1a2a529c0c1
archi-tektur <kontakt@archi-tektur.pl>
2019-03-20 02:16:35 +0100

    Mastered the animation

--- public/scripts/panel.js
+++ public/scripts/panel.js
@@ -2,9 +2,6 @@ const button = document.getElementById('menuSizeSwitcher');
 const navigation = document.getElementById('navigation');
 const classToToggle = 'nav--small';
 
-button.addEventListener('dblclick', () => {
+button.addEventListener('click', () => {
   navigation.classList.toggle(classToToggle);
 });
-
-
-
