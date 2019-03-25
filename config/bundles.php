5b1aa6e5e79824ad0f19121a8c3ba122058e2eed
archi-tektur <kontakt@archi-tektur.pl>
2019-03-25 22:55:51 +0100

    Installed Encore

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class                          => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => true],
 ];
--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\Article;
 use App\Entity\Category;
 use App\Service\EntityService\CategoryService;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -35,12 +37,13 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('title')
-                ->add('content')
+                ->add('content', CKEditorType::class)
                 ->add('showreelImage', FileType::class, ['required' => false])
                 ->add('categories', EntityType::class, [
                     'multiple' => true,
                     'class'    => Category::class,
-                ]);
+                ])
+                ->add('submit', SubmitType::class);
     }
 
     /** @inheritdoc */
