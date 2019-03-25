d730f230f448f686bb3dd6f4525cb3c45e325d5c
archi-tektur <kontakt@archi-tektur.pl>
2019-03-25 22:06:34 +0100

    Fixed translations problems

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class                          => ['all' => true],
 ];
