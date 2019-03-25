de8bca6661fd2eabce13c2186063d63c0676e4d4
archi-tektur <kontakt@archi-tektur.pl>
2019-03-25 22:06:34 +0100

    Fixed translations problems

--- src/Controller/GUI/AdminPanel/CategoryController.php
+++ src/Controller/GUI/AdminPanel/CategoryController.php
@@ -9,6 +9,7 @@ use App\Service\EntityService\CategoryService;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +21,8 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CategoryController extends AbstractController
 {
+    public const FLASH_PREFIX = 'category_';
+
     /**
      * @var CategoryService
      */
@@ -45,27 +48,20 @@ class CategoryController extends AbstractController
     public function index(Request $request): Response
     {
         // create form
-        $form = $this->createForm(CategoryFormType::class);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var Category $category */
-            $category = $form->getData();
-            $this->entityManager->persist($category);
-            $this->entityManager->flush();
-            $this->addFlash('success', 'OK');
-        } elseif ($form->isSubmitted() && !$form->isValid()) {
-            foreach ($form->getErrors(true) as $error) {
-                $this->addFlash('warning', $error->getMessage());
-            }
-        }
+        $addForm = $this->createForm(CategoryFormType::class);
+        $editForm = $this->createForm(CategoryFormType::class);
+        $addForm->handleRequest($request);
+        // handle further request
+        $this->add($addForm);
 
         // download after addition
         $categories = $this->categoryService->getAll();
-        $form = $this->createForm(CategoryFormType::class);
+        $addForm = $this->createForm(CategoryFormType::class);
 
         return $this->render('admin/panels/categories.html.twig', [
-            'categories'      => $categories,
-            'addCategoryForm' => $form->createView(),
+            'categories'       => $categories,
+            'addCategoryForm'  => $addForm->createView(),
+            'editCategoryForm' => $editForm->createView(),
         ]);
     }
 
@@ -79,14 +75,41 @@ class CategoryController extends AbstractController
     public function delete(string $name): Response
     {
         try {
+            // try to delete
             $this->categoryService->delete($name);
         } catch (CategoryNotFoundException $e) {
-            $this->addFlash('warning', 'Category not found!');
+            // when category is not found
+            $this->addFlash(self::FLASH_PREFIX . 'warning', 'Category not found!');
         } catch (ORMException $e) {
             $this->addFlash('warning', 'Database problem occured');
         } finally {
             return $this->redirectToRoute('gui__admin_categories_index');
         }
 
+    }
+
+    /**
+     * Adds category or adds error
+     *
+     * @param FormInterface $addForm
+     */
+    private function add(FormInterface $addForm): void
+    {
+        // when form is valid
+        if ($addForm->isSubmitted() && $addForm->isValid()) {
+            // Handling add form
+            /** @var Category $category */
+            $category = $addForm->getData();
+            $this->entityManager->persist($category);
+            $this->entityManager->flush();
+            $this->addFlash(self::FLASH_PREFIX . 'success', 'OK');
+        }
+
+        // when form is not valid
+        if ($addForm->isSubmitted() && !$addForm->isValid()) {
+            foreach ($addForm->getErrors(true) as $error) {
+                $this->addFlash(self::FLASH_PREFIX . 'warning', $error->getMessage());
+            }
+        }
     }
 }
