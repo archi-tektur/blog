8594dc155edad99ddb64053dc3e24499ffe9bd7a
archi-tektur <kontakt@archi-tektur.pl>
2019-04-06 19:43:23 +0200

    Installed KnpTimeBundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class                           => ['all' => true],
 ];
