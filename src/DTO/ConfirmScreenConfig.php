d66fb11ecdbda15dea391d5aafe523f1ab14e3dc
archi-tektur <kontakt@archi-tektur.pl>
2019-04-07 15:11:32 +0200

    Created simplest Confirm screen config

---
+++ src/DTO/ConfirmScreenConfig.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\DTO\ConfirmScreenConfig;
+
+class ConfirmScreenConfig
+{
+    private $title;
+
+    private $content;
+
+    private $buttons = [];
+}
